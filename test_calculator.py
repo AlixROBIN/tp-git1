8b44781a70b3703dab1341aba4a913646f6ceeed
Alix Robin <alix.robin@esme.fr>
2024-04-25 16:24:35 +0200

    test all

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,20 @@
 from calculator import addition
-from calculator import soustraction
-from calculator import division
+
+
 
 def test_addition():
     assert addition(1, 2) == 3
-def test_soustraction():
-    assert soustraction(1, 2) == 1
+
+from calculator import division
+
+
+
 def test_division():
-    assert division(1,2)==2
+    assert (1, 2) == 2
+
+from calculator import soustraction
+
+
+
+def test_addition():
+    assert soustraction(1, 2) == 1
